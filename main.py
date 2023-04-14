3c5f2cd6fec6e64363ebc00475cf35619f081992
Adria Barreto <adriabarreto12@gmail.com>
2023-04-14 10:44:51 -0300

    Exercício corrigido

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ numero = int(input("Digite um número inteiro: "))
 numero = int(numero)
 if numero <= 0:
     print("Número inválido")
-if numero == 1: 
+elif numero == 1: 
     print("Não primo")
 else: 
     for i in range(2,numero):
